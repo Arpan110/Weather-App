36ac27de0568be131e635bc0c7cee9300fdbe000
Arpan Paul <arpan0325@gmail.com>
2024-07-30 00:46:15 +0530

    my first upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{a570d58dff664446a8b60bee7dfe79fb}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
